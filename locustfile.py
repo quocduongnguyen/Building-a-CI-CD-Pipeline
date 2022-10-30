c3ce60449fca871053e5089ff91265f226dc0acc
Quoc Duong <Quocduong.th10b.mta@gmail.com>
2022-10-30 07:00:06 +0700

    Update locustfile.py

--- locustfile.py
+++ locustfile.py
@@ -2,8 +2,8 @@ from locust import HttpUser, between, task
 
 class WebsiteUser(HttpUser):
     host = "https://duongnq9-project2-webapp.azurewebsites.net:443"
-    wait_time = between(5, 15)
+    wait_time = between(5, 10)
 
     @task
-    def index(self):
+    def task1(self):
         self.client.get("/")
